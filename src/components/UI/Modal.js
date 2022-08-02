c487fb276c24ab8448963acadf7959d6edface39
deasequana-ux <yucehilal99@gmail.com>
2022-08-02 15:08:07 +0300

    develop modal , mealItem and card

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import ReactDOM  from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+// ReactDOM.createPortal(child,container)
+// child render edilmesini istediğimiz component , container ise , bu child componentin render edilmesini istediğimiz DOM elemanıdır.
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)} 
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
